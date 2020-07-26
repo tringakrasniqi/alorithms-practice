d5d71005502a9244fa19d5325c54d88a0d0d9f58
tringak <tringaakrasniqi@gmail.com>
2020-07-26 18:25:12 +0200

    Added single linked lists, add/remove Front, contains

---
+++ sinlge-linked-lists/contains.js
@@ -0,0 +1,63 @@
+class Node {
+      constructor(value) {
+            this.value = value;
+            this.next = null;
+      }
+}
+
+class SSL {
+      constructor(value) {
+            this.head = new Node(value)
+      }
+
+      front(){
+            if (this.head == null) {
+                  return null
+            } else {
+                  return this.head.value
+            }
+      }
+
+      addFront(value) {
+            if (this.front() == null) {
+                  SSL(value)
+            } else {
+                  var new_node = new Node(value);
+                  new_node.next = this.head
+                  this.head = new_node
+            }
+            return this
+      }
+
+      removeFront() {
+            if (this.front() == null) {
+                  return null
+            } else {
+                  this.head = this.head.next
+                  return this
+            }
+      }
+
+      contains(value) {      
+            var node = this.head;
+            while(node) {
+                if(node.value == value) {
+                    return true
+                }
+                node = node.next
+            }
+            return this
+      }
+}
+
+var ssl = new SSL(2)
+ssl.addFront(5)
+ssl.addFront(6)
+ssl.addFront("Hi")
+ssl.addFront(3)
+console.log(ssl)
+
+console.log(ssl.contains("Hi"))
+console.log(ssl.contains(2))
+console.log(ssl.contains(10))
+console.log(ssl.contains(5))
