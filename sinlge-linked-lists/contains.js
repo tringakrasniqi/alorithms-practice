157bdcbab113992320ab482dfe9e2f9a9679af84
tringak <tringaakrasniqi@gmail.com>
2020-07-28 08:55:29 +0200

    SList secondToLast, removeSelf, copyList, filter

--- sinlge-linked-lists/contains.js
+++ sinlge-linked-lists/contains.js
@@ -131,6 +131,57 @@ class SSL {
             runner.next = null
             return this
       }
+
+      secondToLast() {
+            var runner = this.head
+            if (!runner.next) {
+                  return null 
+            }
+
+            while(runner.next.next) {
+                  runner = runner.next
+            }
+            return runner
+      }
+
+      removeSelf(node) {
+            var runner = this.head
+            while(runner.next) {
+                  if (runner.next == node) {
+                        runner.next = runner.next.next
+                        runner.next.next = null
+                        return runner.next
+                  }
+                  runner = runner.next
+            }
+            return this.head
+      }
+
+      copyList() {
+            var runner = this.head
+            var new_head = new SSL(runner.value)
+            while(runner) {
+                  var node = new Node(runner.value)
+                  node.next = new_head
+                  new_head = node
+                  runner = runner.next
+            }
+            return new_head
+      }
+
+      filter(min, max) {
+            var runner = this.head
+            while(runner.next.next) {
+                  if (runner.value < min || runner.value > max) {
+                        runner.next = runner.next.next
+                        // runner.next.next = null
+                        return runner.next
+                  }
+                  runner = runner.next
+            }
+            return this
+
+      }
 }
 
 var ssl = new SSL(2)
@@ -139,14 +190,21 @@ ssl.addFront(-6)
 ssl.addFront(3)
 ssl.addFront(10)
 
-// console.log(ssl)
+console.log(ssl.filter(0, 11))
 
 // console.log(ssl.contains("Hi"))
 // console.log(ssl.contains(2))
 // console.log(ssl.contains(10))
 // console.log(ssl.contains(5))
 
-ssl.addBack(20)
-console.log(ssl.back())
-ssl.removeBack()
-console.log(ssl.back())
+// ssl.addBack(20)
+// console.log(ssl.secondToLast())
+// ssl.display()
+// ssl.removeSelf(ssl.secondToLast())
+// ssl.removeSelf(ssl.secondToLast())
+
+ssl.display()
+// ssl.removeBack()
+// console.log(ssl.back())
+
+// ssl.copyList()
